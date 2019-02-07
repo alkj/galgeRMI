6952a2dfac1bc9bef52d8870e4a0728bc84acb65
alexander <alexander@alexander-ThinkPad-T530>
2019-02-07 23:13:18 +0100

    småting

--- galgeRMI/src/galge/Galge.java
+++ galgeRMI/src/galge/Galge.java
@@ -8,14 +8,6 @@ package galge;
 
 
 /**
- * Spørgsmål fra undervisningen.
- * Spørgsmål 1: Ender saldoen stadig på det forventede ?
- *      nej. det virker som om at der bliver lavet beregninger sammentidigt, 
- *      med at forespørgslerne kommer. så hvis de kommer oven i hinanden, så bliver det rodet.
- * 
- * Spørgsmål 2: Gør det nogen forskel hvor ventetiden (Thread.sleep(500)) står ?
- *      ja. det gør en stor forskel. man kunne sikre sig at serveren bliver færdig med en klient,
- *      før den starter med den næste.
  * 
  * @author alexander
  */
